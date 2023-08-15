4d8adebf69e331b2821a326927e70d96d0220041
N4K <pronin6030@gmail.com>
2023-08-15 18:47:27 +0300

    wip

---
+++ DangerousD/GameCore/GameObjects/LivingEntities/Monsters/Frank.cs
@@ -0,0 +1,53 @@
+﻿using DangerousD.GameCore.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DangerousD.GameCore.GameObjects.LivingEntities.Monsters
+{
+    internal class Frank : CoreEnemy
+    {
+        private bool isGoRight = false;
+        public Frank(Vector2 position) : base(position)
+        {
+            Width = 56;
+            Height = 80;
+            GraphicsComponent.StartAnimation("FrankMoveLeft");
+            monster_speed = 50;
+        }
+        protected override GraphicsComponent GraphicsComponent { get; } = new(new List<string> { "FrankMoveRight", "FrankMoveLeft" }, "FrankMoveRight");
+
+        public override void Attack()
+        {
+
+        }
+
+        public override void Death()
+        {
+
+        }
+
+        public override void Move(GameTime gameTime)
+        {
+            if (isGoRight)
+            {
+                if (GraphicsComponent.GetCurrentAnimation != "FrankMoveRight")
+                {
+                    GraphicsComponent.StartAnimation("FrankMoveRight");
+                }
+            }
+            else if (!isGoRight)
+            {
+                if (GraphicsComponent.GetCurrentAnimation != "FrankMoveLeft")
+                {
+                    GraphicsComponent.StartAnimation("FrankMoveLeft");
+                }
+            }
+        }
+    }
+}
---
+++ DangerousD/GameCore/GameObjects/LivingEntities/Monsters/Spider.cs
@@ -0,0 +1,36 @@
+﻿using DangerousD.GameCore.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DangerousD.GameCore.GameObjects.LivingEntities.Monsters
+{
+    public class Spider : CoreEnemy
+    {
+        public Spider(Vector2 position) : base(position)
+        {
+        }
+
+        protected override GraphicsComponent GraphicsComponent { get; } = new(new List<string> { "SpiderMoveRight", "SpiderMoveLeft", "SpiderDown", "SpiderUp" }, "");
+
+        public override void Attack()
+        {
+
+        }
+
+        public override void Death()
+        {
+
+        }
+
+        public override void Move(GameTime gameTime)
+        {
+
+        }
+    }
+}
