8477a3a2745bccfed5e8b1dace49c25fe6ad4ba5
gravity <dvaerkagama@gmail.com>
2023-08-15 11:34:46 +0300

    ui

--- DangerousD/GameCore/GUI/MenuGUI.cs
+++ DangerousD/GameCore/GUI/MenuGUI.cs
@@ -15,7 +15,7 @@ internal class MenuGUI : AbstractGui
         {
             AppManager.Instance.ChangeGameState(GameState.Game);
         };
-        Elements.Add(new Button(Manager) { rectangle = new Rectangle(0, 10, 790, 50) });
+        Elements.Add(new Button(Manager) { rectangle = new Rectangle(0, 10, 50, 50) });
         Elements.Add(new Label(Manager) { rectangle = new Rectangle(100, 10, 50, 50), text = "DA" });
     }
 }
