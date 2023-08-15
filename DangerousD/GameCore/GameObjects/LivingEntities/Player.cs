b423ac2b498220ad1fb2a85eaab233cccb2e7808
SergoDobro <optimgrey@yandex.ru>
2023-08-15 14:38:01 +0300

    prepared for origin but no refactoring

--- DangerousD/GameCore/GameObjects/LivingEntities/Player.cs
+++ DangerousD/GameCore/GameObjects/LivingEntities/Player.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DangerousD.GameCore.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,10 @@ using System.Threading.Tasks;
 
 namespace DangerousD.GameCore.GameObjects.LivingEntities
 {
-    public class Player
+    public class Player : LivingEntity
     {
+        protected override GraphicsComponent GraphicsComponent => throw new NotImplementedException();
+
         public void Kill()
         {
 
--- DangerousD/GameCore/Managers/SettingsManager.cs
+++ DangerousD/GameCore/Managers/SettingsManager.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DangerousD.GameCore.Graphics
+namespace DangerousD.GameCore.Managers
 {
     internal class SettingsManager
     {
