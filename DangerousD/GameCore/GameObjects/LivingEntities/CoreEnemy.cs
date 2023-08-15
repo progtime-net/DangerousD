f2a6cdbd8d15fd42352e1c12b9ddcfb03ab3bb46
bmvolf <bmvolgin@gmail.com>
2023-08-15 15:22:52 +0300

    wip

---
+++ DangerousD/GameCore/GameObjects/LivingEntities/CoreEnemy.cs
@@ -0,0 +1,39 @@
+﻿using DangerousD.GameCore.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DangerousD.GameCore.GameObjects.LivingEntities
+{
+    public abstract class CoreEnemy : LivingEntity
+    {
+        protected int monster_health;
+        protected int monster_speed;
+        protected string name;
+        protected bool isAlive = true;
+
+        public CoreEnemy(Vector2 position) : base(position)
+        {
+            //здесь я не понял
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (monster_health <= 0)
+            {
+                Death();
+            }
+            base.Update(gameTime);
+        }
+        public abstract void Death();
+
+        public abstract void Attack();
+
+        public abstract void Move();
+    }
+}
---
+++ DangerousD/GameCore/GameObjects/LivingEntities/Monsters/Zombie.cs
@@ -0,0 +1,47 @@
+﻿using DangerousD.GameCore.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DangerousD.GameCore.GameObjects.LivingEntities.Monsters
+{
+    public class Zombie : CoreEnemy
+    {
+        public Zombie(Vector2 position) : base(position)
+        {
+            Width = 24;
+            Height = 40;
+            GraphicsComponent.StartAnimation("ZombieMoveRight");
+        }
+        protected override GraphicsComponent GraphicsComponent { get; } = new(new List<string> { "ZombieMoveRight"}, "ZombieMoveRight");
+
+        public override void Update(GameTime gameTime)
+        {
+            Move();
+            if (monster_health <= 0)
+            {
+                Death();
+            }
+            base.Update(gameTime);
+        }
+
+        public override void Attack()
+        {
+
+        }
+
+        public override void Death()
+        {
+
+        }
+
+        public override void Move()
+        {
+        }
+    }
+}
