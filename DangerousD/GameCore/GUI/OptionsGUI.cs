48f89a72fd0454c353660daa0a9226e688f94300
MARKPRO44 <119742977+MARKPRO4444@users.noreply.github.com>
2023-08-16 16:01:34 +0300

    started options gui

--- DangerousD/GameCore/GUI/OptionsGUI.cs
+++ DangerousD/GameCore/GUI/OptionsGUI.cs
@@ -10,8 +10,18 @@ namespace DangerousD.GameCore.GUI
 	{
         protected override void CreateUI()
         {
-            var slider = new Slider(Manager) { };
+            var slider = new Slider(Manager)
+            {
+                MinValue = 0,
+                MaxValue = 1,
+                rectangle = new Rectangle(650, 150, 100 ,40)
+            };
 
+            Label lblOptions = new Label(Manager, 0);
+            lblOptions.fontName = "font2";
+            lblOptions.text = "Options";
+            lblOptions.rectangle = new Rectangle(300, 20, 210, 50);
+            
         }
     } 
 }
