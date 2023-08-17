005ce1cfb8d2fdba92c99a805b4c9505aa8636e5
AnloGames <7383an@gmail.com>
2023-08-17 18:08:33 +0300

    NetworkManager Sound fix

--- DangerousD/GameCore/Managers/SoundManager.cs
+++ DangerousD/GameCore/Managers/SoundManager.cs
@@ -44,7 +44,7 @@ namespace DangerousD.GameCore
             sound.SoundEffect.IsLooped = false;
             sound.SoundEffect.Play();
             PlayingSounds.Add(sound);
-            if (AppManager.Instance.multiPlayerStatus != MultiPlayerStatus.Host)
+            if (AppManager.Instance.multiPlayerStatus == MultiPlayerStatus.Host)
             {
                 AppManager.Instance.NetworkManager.SendMsg(new Network.NetworkTask(Vector2.Zero, soundName));
             }
@@ -56,7 +56,7 @@ namespace DangerousD.GameCore
             sound.SoundEffect.Volume = (float)sound.GetDistance(playerPos) / MaxSoundDistance;
             sound.SoundEffect.Play();
             PlayingSounds.Add(sound);
-            if (AppManager.Instance.multiPlayerStatus != MultiPlayerStatus.Host) 
+            if (AppManager.Instance.multiPlayerStatus == MultiPlayerStatus.Host) 
             {
                 AppManager.Instance.NetworkManager.SendMsg(new Network.NetworkTask(soundPos, soundName));
             }
