087472ddc391eb568f08d0e84c86dd4df4d63218
Timofey06 <tima.loktionov@yandex.ru>
2023-08-17 16:48:29 +0300

    ColisionTracerOn

--- DangerousD/GameCore/GameObjects/MapObjects/StopTile.cs
+++ DangerousD/GameCore/GameObjects/MapObjects/StopTile.cs
@@ -1,5 +1,6 @@
 using DangerousD.GameCore.Graphics;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace DangerousD.GameCore.GameObjects.MapObjects;
 
@@ -8,5 +9,12 @@ public class StopTile : MapObject
     public override bool IsColliderOn { get; protected set; } = true;
     public StopTile(Vector2 position, Vector2 size, Rectangle sourceRectangle) : base(position, size, sourceRectangle)
     {
+    }
+    public override void Draw(SpriteBatch spriteBatch)
+    {
+        base.Draw(spriteBatch);
+        //debug
+        spriteBatch.Draw(debugTexture, new Rectangle(Rectangle.X - GraphicsComponent.CameraPosition.X, Rectangle.Y - GraphicsComponent.CameraPosition.Y, Rectangle.Width, Rectangle.Height), Color.White);
+
     }
 }
