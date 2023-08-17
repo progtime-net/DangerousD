7f981ccd5ac4d572d9a08cc570bebb114f06bc75
gravity <dvaerkagama@gmail.com>
2023-08-17 20:58:11 +0300

    ui

--- DangerousD/GameCore/GUI/HUD.cs
+++ DangerousD/GameCore/GUI/HUD.cs
@@ -5,36 +5,9 @@ using MonogameLibrary.UI.Elements;
 
 namespace DangerousD.GameCore.GUI
 {
-	public class HUD : IDrawableObject
-	{
-        
-
-		public HUD()
-		{
-		}
-
-        public void Draw(SpriteBatch spriteBatch)
-        {
-            ;
-        }
-
-        public void Initialize(GraphicsDevice graphicsDevice)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Initialize()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void LoadContent()
-        {
-            var content = AppManager.Instance.Content;
-
-        }
-
-        public void Update(GameTime gameTime)
+    public class HUD : AbstractGui
+    {
+        protected override void CreateUI()
         {
             throw new NotImplementedException();
         }
