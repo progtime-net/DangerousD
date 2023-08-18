87c3a168af698f84235e87d322271fe38248717c
Ivan Filipenkov <hello@vanutp.dev>
2023-08-18 19:59:32 +0300

    fix

--- DangerousD/GameCore/Managers/AppManager.cs
+++ DangerousD/GameCore/Managers/AppManager.cs
@@ -208,14 +208,7 @@ namespace DangerousD.GameCore
                 case GameState.Lobby:
                     break;
                 case GameState.Game:
-<<<<<<< HEAD
-
-                    GameManager.mapManager.LoadLevel("map");
-
-                    
-=======
                     GameManager.mapManager.LoadLevel(currentMap);
->>>>>>> main
                     GameManager.FindBorders();
                     break;
                 case GameState.Death:
