d87770f9ec15069794438bf2237eadbf66be1d5a
Kaktus200020 <artemkremnev2007@gmail.com>
2023-08-18 17:19:44 +0300

    WereWolfAdding

--- DangerousD/GameCore/Managers/AppManager.cs
+++ DangerousD/GameCore/Managers/AppManager.cs
@@ -201,7 +201,7 @@ namespace DangerousD.GameCore
                     break;
                 case GameState.Game:
 
-                    GameManager.mapManager.LoadLevel("lvl");
+                    GameManager.mapManager.LoadLevel("map");
 
                     
                     GameManager.FindBorders();
