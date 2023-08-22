735c16a0003c3aa2c25cbd2da12dcb12c2d37abe
SergoDobro <optimgrey@yandex.ru>
2023-08-23 00:56:34 +0300

    SHADERS FINALLY WORK!

---
+++ DangerousD/Content/Shaders/Glow.fx
@@ -0,0 +1,65 @@
+﻿#if OPENGL
+#define SV_POSITION POSITION
+#define VS_SHADERMODEL vs_3_0
+#define PS_SHADERMODEL ps_3_0
+#else
+#define VS_SHADERMODEL vs_4_0_level_9_1
+#define PS_SHADERMODEL ps_4_0_level_9_1
+#endif
+
+
+Texture2D SpriteTexture;
+
+sampler2D SpriteTextureSampler = sampler_state
+{
+    Texture = <SpriteTexture>;
+};
+
+struct VertexShaderOutput
+{
+    float4 Position : SV_POSITION;
+    float4 Color : COLOR0;
+    float2 TextureCoordinates : TEXCOORD0;
+};
+
+float4 MainPS(VertexShaderOutput input) : COLOR
+{
+    float blurDistanceX = 0.001;//1 / 1366.0;
+    float blurDistanceY = blurDistanceX; //1 / 768.0;
+    float3 color = tex2D(SpriteTextureSampler, input.TextureCoordinates.xy).rgb;
+    
+    color =  tex2D(SpriteTextureSampler, float2(input.TextureCoordinates.x, input.TextureCoordinates.y));
+    int k = 2;
+    for (int i = -k; i < k+1; i++)
+    {
+        for (int j = -k; j < k + 1; j++)
+        {
+            color += tex2D(SpriteTextureSampler, float2(input.TextureCoordinates.x + i * blurDistanceX, input.TextureCoordinates.y + j * blurDistanceY));
+        }
+    }
+    //color = tex2D(SpriteTextureSampler, float2(input.TextureCoordinates.x + blurDistance, input.TextureCoordinates.y + blurDistance));
+    //color += tex2D(SpriteTextureSampler, float2(input.TextureCoordinates.x - blurDistance, input.TextureCoordinates.y - blurDistance));
+    //color += tex2D(SpriteTextureSampler, float2(input.TextureCoordinates.x + blurDistance, input.TextureCoordinates.y - blurDistance));
+    //color += tex2D(SpriteTextureSampler, float2(input.TextureCoordinates.x - blurDistance, input.TextureCoordinates.y + blurDistance)); 
+    color = color / ((2 * k + 1) * (2 * k + 1) + 1); // - tex2D(SpriteTextureSampler, float2(input.TextureCoordinates.x , input.TextureCoordinates.y )).xyz;
+    color -= tex2D(SpriteTextureSampler, float2(input.TextureCoordinates.x, input.TextureCoordinates.y));
+    color *= 1.52;
+    color.r += tex2D(SpriteTextureSampler, float2(input.TextureCoordinates.x + 5 * blurDistanceX, input.TextureCoordinates.y)); 
+    
+    float bord = 0.95;
+    if (color.r < bord)
+        color.r = tex2D(SpriteTextureSampler, input.TextureCoordinates.xy).r;
+    if (color.g < bord)
+        color.g = tex2D(SpriteTextureSampler, input.TextureCoordinates.xy).g;
+    if (color.b < bord)
+        color.b = tex2D(SpriteTextureSampler, input.TextureCoordinates.xy).b;
+        //color += 0.5 * tex2D(SpriteTextureSampler, input.TextureCoordinates.xy).rgb;
+    return float4(color, 1);  
+}
+technique Blur
+{
+    pass P0
+    {
+        PixelShader = compile PS_SHADERMODEL MainPS();
+    }
+};
--- DangerousD/GameCore/Managers/AppManager.cs
+++ DangerousD/GameCore/Managers/AppManager.cs
@@ -17,7 +17,9 @@ using System.Threading.Tasks;
 namespace DangerousD.GameCore
 {
     public enum MultiPlayerStatus { SinglePlayer, Host, Client }
-    public enum GameState { Menu, Options, Lobby, Game, Login, Death, HUD,
+    public enum GameState
+    {
+        Menu, Options, Lobby, Game, Login, Death, HUD,
         GameOver
     }
     public class AppManager : Game
@@ -30,7 +32,7 @@ namespace DangerousD.GameCore
         public MultiPlayerStatus multiPlayerStatus { get; private set; } = MultiPlayerStatus.SinglePlayer;
         public Point resolution;
         public Point inGameResolution = new Point(1920, 1080);
-        public Point inGameHUDHelperResolution= new Point(1920, 1080);
+        public Point inGameHUDHelperResolution = new Point(1920, 1080);
         IDrawableObject MenuGUI;
         IDrawableObject OptionsGUI;
         IDrawableObject LoginGUI;
@@ -48,6 +50,7 @@ namespace DangerousD.GameCore
         public SettingsManager SettingsManager { get; private set; } = new SettingsManager();
 
         private RenderTarget2D renderTarget;
+        Effect spriteEffect;
         public AppManager()
         {
             Content.RootDirectory = "Content";
@@ -55,6 +58,7 @@ namespace DangerousD.GameCore
             _graphics = new GraphicsDeviceManager(this);
             IsMouseVisible = true;
             TargetElapsedTime = TimeSpan.FromMilliseconds(1000 / 30);
+            _graphics.GraphicsProfile = GraphicsProfile.HiDef;
 
             SettingsManager = new SettingsManager();
             SettingsManager.LoadSettings();
@@ -78,6 +82,7 @@ namespace DangerousD.GameCore
             UIManager.resolution = resolution;
             UIManager.resolutionInGame = inGameResolution;
             currentMap = "lvl";
+             
         }
 
         protected override void Initialize()
@@ -94,7 +99,7 @@ namespace DangerousD.GameCore
             base.Initialize();
         }
 
-        protected override void LoadContent() 
+        protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             DebugHUD.LoadContent();
@@ -105,10 +110,12 @@ namespace DangerousD.GameCore
             DeathGUI.LoadContent();
             HUD.LoadContent();
             GameObject.debugTexture = new Texture2D(GraphicsDevice, 1, 1);
-            GameObject.debugTexture.SetData<Color>(new Color[] { new Color(1, 0,0,0.25f) });
+            GameObject.debugTexture.SetData<Color>(new Color[] { new Color(1, 0, 0, 0.25f) });
             SoundManager.LoadSounds();
-            SoundManager.StartAmbientSound("DoomTestSong"); 
+            SoundManager.StartAmbientSound("DoomTestSong");
             renderTarget = new RenderTarget2D(GraphicsDevice, inGameResolution.X, inGameResolution.Y);
+            spriteEffect = Content.Load<Effect>("Shaders//Glow");
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -143,7 +150,7 @@ namespace DangerousD.GameCore
                 case GameState.Game:
                     HUD.Update(gameTime);
                     GameManager.Update(gameTime);
-                    
+
                     break;
                 default:
                     break;
@@ -156,7 +163,7 @@ namespace DangerousD.GameCore
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-             
+
             GraphicsDevice.SetRenderTarget(renderTarget);
             switch (gameState)
             {
@@ -179,7 +186,7 @@ namespace DangerousD.GameCore
                     HUD.Draw(_spriteBatch);
                     break;
                 case GameState.Game:
-                    _spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp);
+                    _spriteBatch.Begin(SpriteSortMode.Immediate, null, SamplerState.PointClamp);
                     GameManager.Draw(_spriteBatch);
                     _spriteBatch.End();
                     HUD.Draw(_spriteBatch);
@@ -188,9 +195,26 @@ namespace DangerousD.GameCore
                     break;
             }
             GraphicsDevice.SetRenderTarget(null);
-            _spriteBatch.Begin();
-            _spriteBatch.Draw(renderTarget, new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight), Color.White);
+            
+            spriteEffect.CurrentTechnique = spriteEffect.Techniques["Blur"];
+
+            _spriteBatch.Begin(effect:spriteEffect);
+
+            //_spriteBatch.Draw(renderTarget, new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight), Color.White);
+            GraphicsDevice.BlendState = BlendState.Additive;
+            foreach (EffectPass pass in spriteEffect.CurrentTechnique.Passes)
+            {
+                _spriteBatch.Draw(renderTarget, new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight), Color.White);
+                pass.Apply(); 
+            }
             _spriteBatch.End();
+
+
+
+
+            #region test 
+            #endregion
+
 
 
             DebugHUD.Draw(_spriteBatch);
@@ -249,20 +273,20 @@ namespace DangerousD.GameCore
                         }
                         else if (networkTask.type == typeof(Particle))
                         {
-                                Particle particle = new Particle(networkTask.position);
-                                particle.id = networkTask.objId;    
-                                particle.velocity = networkTask.velocity;
+                            Particle particle = new Particle(networkTask.position);
+                            particle.id = networkTask.objId;
+                            particle.velocity = networkTask.velocity;
                         }
                         break;
                     case NetworkTaskOperationEnum.SendPosition:
-                        if (networkTask.objId != GameManager.GetPlayer1.id )
+                        if (networkTask.objId != GameManager.GetPlayer1.id)
                         {
                             LivingEntity entity = GameManager.livingEntities.Find(x => x.id == networkTask.objId);
                             if (entity != null)
                                 entity.SetPosition(networkTask.position);
                             if (multiPlayerStatus == MultiPlayerStatus.Host)
                             {
-                                NetworkTasks.Add(networkTask);  
+                                NetworkTasks.Add(networkTask);
                             }
                         }
                         break;
@@ -309,7 +333,7 @@ namespace DangerousD.GameCore
                         remoteConnectedPlayer.GetGraphicsComponent().parentId = networkTask.objId;
                         break;
                     case NetworkTaskOperationEnum.KillPlayer:
-                        Player player1 = GameManager.players.Find(x => x.id==networkTask.objId);
+                        Player player1 = GameManager.players.Find(x => x.id == networkTask.objId);
                         player1.Death(networkTask.name);
                         NetworkTask task1 = new NetworkTask();
                         NetworkTasks.Add(task1.DeleteObject(player1.id));
