9b73eff96464957dda8ab4df2eb5c6e6dd3b9f83
Mootfrost777 <andrey13102007@gmail.com>
2023-12-13 21:47:52 +0300

    Fix lvl2 zombie traps

--- DangerousD/GameCore/Managers/AppManager.cs
+++ DangerousD/GameCore/Managers/AppManager.cs
@@ -81,7 +81,7 @@ namespace DangerousD.GameCore
             DebugHUD = new DebugHUD();
             UIManager.resolution = resolution;
             UIManager.resolutionInGame = inGameResolution;
-            currentMap = "lvl1";
+            currentMap = "lvl2";
         }
 
         protected override void Initialize()
