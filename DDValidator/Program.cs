3ab59b4e3e179e88f11b71fa86213a7e33e188cb
Mootfrost777 <andrey13102007@gmail.com>
2023-12-18 19:27:23 +0300

    Extension works

--- DDValidator/Program.cs
+++ DDValidator/Program.cs
@@ -2,15 +2,11 @@
 
 // Force load assembly
 var a = GameObject.debugTexture;
-
 string path = Console.ReadLine();
 while (path != "END")
 {
     bool f = false;
     var type = Type.GetType($"DangerousD.GameCore.GameObjects.{path}, DangerousD");
-    if (type is not null)
-        Console.WriteLine("OK");
-    else
-        Console.WriteLine("NF");
+    Console.Write(type is not null ? "OK" : "NF");
     path = Console.ReadLine();
 }
--- TiledValidator/check.js
+++ TiledValidator/check.js
@@ -1,81 +1,138 @@
 let objectErrors = []
 let layerErrors = []
-
+let errCount
+let cur = 0
+let map
 
 function getGamePath() {
     let file = new TextFile(`${tiled.project.extensionsPath}/.DDValidator`)
-    return file.readLine()
+    let res = file.readLine()
+    file.close()
+    return res
 }
 
 
 var checkMistakes = tiled.registerAction("MistakesCheck", function(action) {
-    const map = tiled.activeAsset;
+    objectErrors = []
+    layerErrors = []
+    cur = []
+    map = tiled.activeAsset;
     if (!map.isTileMap) {
         tiled.alert("Not a tile map!");
         return;
     }
 
+    // TODO: first object returns nothing, shiza!!!!!!!
     var prc = new Process()
     prc.start(getGamePath(), [])
     for (let i = map.layerCount - 1; i >= 0; i--) {
 
         const layer = map.layerAt(i);
         if (layer.isObjectLayer) {
-
             for (let i = layer.objectCount - 1; i >= 0; i--) {
                 const obj = layer.objectAt(i)
-                let cln = layer.className ? layer.className : "" + obj.className ? `.${obj.className}` : ""
+                let cln = layer.className ? layer.className : ""
+                if (obj.className){
+                    if (cln) cln += `.${obj.className}`
+                    else cln = obj.className
+                }
+
                 if (cln === "") {
-                    objectErrors.push(obj.id)
+                    objectErrors.push(obj)
                     continue
                 }
                 prc.writeLine(cln)
-                if (prc.readLine() !== "OK") {
-                    if (!obj.className){
-                        layerErrors.push(layer.id)
+                prc.waitForFinished(100)
+                let ans = prc.readLine()
+                if (ans !== "OK") {
+                    if (obj.className){
+                        layerErrors.push(layer)
                         break
                     }
-                    else objectErrors.push(obj.id)
+                    else objectErrors.push(obj)
                 }
             }
         }
         else if (layer.isTileLayer) {
+            if (layer.className === "") {
+                layerErrors.push(layer)
+                continue
+            }
             prc.writeLine(layer.className)
-            if (prc.readLine() !== "OK") layerErrors.push(layer.id)
+            prc.waitForFinished(100)
+
+            let ans = prc.readLine()
+            if (ans !== "OK") layerErrors.push(layer)
         }
     }
     prc.writeLine("END")
+    prc.close()
+    errCount = layerErrors.length + objectErrors.length
+    if (errCount > 0) {
+        let resolve = tiled.confirm(`${layerErrors.length} layers and ${objectErrors.length} objects found whose classes do not exist in the game. Start resolving?`)
+
+        if (resolve){
+            next.enabled = true
+            hc.enabled = true
+
+            tiled.trigger("HighlightCur")
+        }
+    }
+    else tiled.alert("No errors found!")
 })
 checkMistakes.text = "Check map for mistakes"
 checkMistakes.shortcut = "Ctrl+Shift+C"
 
-var prevMistake = tiled.registerAction("PrevMistake", function(action) {
+var hc = tiled.registerAction("HighlightCur", function(action) {
+    if (cur < layerErrors.length){
+        map.currentLayer = layerErrors[cur];
+        tiled.log(`Resolving ${cur} error(layer id: ${layerErrors[cur].id})`)
+    }
+    else {
+        let obj = objectErrors[cur - layerErrors.length]
+        tiled.log(`Resolving ${cur} error(object id: ${obj.id})`)
+        const pos = map.pixelToScreen ? map.pixelToScreen(obj.pos) :obj.pos;
+        tiled.mapEditor.currentMapView.centerOn(pos.x, pos.y);
+        map.selectedObjects = [obj];
+    }
 })
-prevMistake.text = "Previous mistake"
-prevMistake.shortcut = "Ctrl+Left"
-prevMistake.enabled = false
+hc.text = "Highlight current"
+hc.shortcut = "Ctrl+Up"
+hc.enabled = false
 
-var highlightCur = tiled.registerAction("HighlightCur", function(action) {
+var next = tiled.registerAction("NextErr", function(action) {
+    cur++
+    if (cur > errCount - 1) cur = 0
+    if (cur > 0) prev.enabled = true
+    tiled.trigger("HighlightCur")
 })
-highlightCur.text = "Highlight current mistake"
-highlightCur.shortcut = "Ctrl+Up"
-highlightCur.enabled = false
+next.text = "Next"
+next.shortcut = "Ctrl+Right"
+next.enabled = false
 
-var nextMistake = tiled.registerAction("NextMistake", function(action) {
+
+var prev = tiled.registerAction("PrevErr", function(action) {
+    cur--
+    if (!cur) prev.enabled = false
+    if (cur < 0) cur = errCount - 1
+    tiled.trigger("HighlightCur")
 })
-nextMistake.text = "Next mistake"
-nextMistake.shortcut = "Ctrl+Right"
-nextMistake.enabled = false
-
-
-
+prev.text = "Prev"
+prev.shortcut = "Ctrl+Left"
+prev.enabled = false
 
 tiled.extendMenu("Map", [
     { action: "MistakesCheck", before: "AutoMap" },
-    { action: "PrevMistake"},
-    { action: "HighlightCur"},
-    { action: "NextMistake"},
+    { action: "PrevErr"},
+    { action: "HighlightCur" },
+    { action: "NextErr"},
     { separator: true }
 ]);
+tiled.activeAssetChanged.connect(() => {
+    next.enabled = false
+    hc.enabled = false
+    prev.enabled = false
+})
+
 
 
