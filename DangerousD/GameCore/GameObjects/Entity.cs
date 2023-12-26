3aa921ae9f59f261a4a4d74fb3fd4fd7b631f7d5
SergoDobro <optimgrey@yandex.ru>
2023-12-26 18:34:59 +0300

    minor undeleted changes deleted

--- DangerousD/GameCore/GameObjects/Entity.cs
+++ DangerousD/GameCore/GameObjects/Entity.cs
@@ -7,8 +7,7 @@ using Microsoft.Xna.Framework.Graphics;
 namespace DangerousD.GameCore.GameObjects
 {
     public abstract class Entity : GameObject
-    {
-        protected bool hasColidedLastUpdateCall = false;
+    { 
         public Entity(Vector2 position) : base(position) {}
         public virtual void SetPosition(Vector2 position) {  _pos = position; } 
         
